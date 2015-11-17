37515a34b6f24439db1e655ece46e75c61d1d415
Alexg2195 <alexg2195@gmail.com>
2015-11-17 01:22:25 -0500

    timer working and images hide and alert when timer is done

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 var imgElements = document.getElementById("pictures");
 var startBtn = document.getElementById("startBtn");
 
+
+function resetGame() {
+  imgElements.style.visibility = "hidden";
+  alert("Well that was fun wasn't it!?!");
+}
+
 startBtn.addEventListener("click", function(){
   imgElements.style.visibility = "visible";
+  setTimeout(resetGame, 2000);
 });
