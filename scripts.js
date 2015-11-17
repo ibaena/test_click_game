653a299fdff59b500511e9a8b1fae36498853a25
Alexg2195 <alexg2195@gmail.com>
2015-11-17 02:09:14 -0500

    attempting to get counting to work ....

--- scripts.js
+++ scripts.js
@@ -1,13 +1,31 @@
 var imgElements = document.getElementById("pictures");
 var startBtn = document.getElementById("startBtn");
+var images = document.getElementsByClassName("img.responsive");
+var myCount = 0;
 
 
 function resetGame() {
   imgElements.style.visibility = "hidden";
-  alert("Well that was fun wasn't it!?!");
+  alert("Well that was fun wasn't it!?! You got " + myCount + " of 18!");
+  // reset all data states here
 }
 
-startBtn.addEventListener("click", function(){
+for(var i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function() {
+    if (this.getAttribute("data-state") === "notPushed") {
+      setAttribute("data-state", "pushed");
+      myCount++;
+    }
+  });
+}
+
+startBtn.addEventListener("click", function() {
   imgElements.style.visibility = "visible";
   setTimeout(resetGame, 2000);
 });
+
+/*function resetGame() {
+  imgElements.style.visibility = "hidden";
+  alert("Well that was fun wasn't it!?!");
+  // reset all data states here
+}
